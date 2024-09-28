df84584ff6e2c8344e4879966bf44586e8436cf6
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-09-28 22:15:11 +0700

    add config store

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { Button } from 'primereact/button';
 import Navbar from './components/Navbar';
 import SearchInput from './components/SearchInput';
 import CardContainer from './components/CardContainer';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
 
-    const [visible, setVisible] = useState(false); 
+    const [visible, setVisible] = useState(false);
 
     return (
         <>
-            <Navbar/>
-            <SearchInput/>
-            <CardContainer/>
-            <Button label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)} />
-            <Modal visible={visible} setVisible={setVisible} />
+            <Provider store={store}>
+                <Navbar />
+                <SearchInput />
+                <CardContainer />
+                <Button label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)} />
+                <Modal visible={visible} setVisible={setVisible} />
+            </Provider>
         </>
     );
 }
---
+++ src/store/slice/counterSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    counter : 0
+};
+
+export const counterSlice = createSlice({
+    name: "counterSlice",
+    initialState,
+    reducers: {
+    },
+})
+
+console.log(counterSlice, "counterslice");
