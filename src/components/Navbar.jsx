ed3ff49ff885c26c42d79a5d98bca4de0085ae55
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-10-05 17:18:17 +0700

    menambahkan logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../assets/logoYNpng.png'
 
 const Navbar = () => {
   return (
-    <div className='py-3 px-5 bg-slate-600'>
+    <div className='py-3 flex gap-2 items-center px-5 bg-slate-600'>
+        <img src={logo} alt="" className='w-7 h-7'/>
         <p className='text-white font-bold text-2xl'>YN Movie</p>
     </div>
   )
